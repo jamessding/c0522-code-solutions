f4e2cb02ed8d112870d9faaa72dd10179997d285
James Ding <jamesding528@yahoo.com>
2022-05-20 17:38:52 +0000

    fixed getIndexes to use push method

--- javascript-loops-practice/get-indexes.js
+++ javascript-loops-practice/get-indexes.js
@@ -1,12 +1,12 @@
 /* exported getIndexes */
 // create an empty array
 // for each value in the given array from index 0 to the final index of the array
-// assign the current index value to the empty array at current index
+// add i to the end of array arr
 // return the final array
 function getIndexes(array) {
   var arr = [];
   for (var i = 0; i < array.length; i++) {
-    arr[i] = i;
+    arr.push(i);
   }
   return arr;
 }
