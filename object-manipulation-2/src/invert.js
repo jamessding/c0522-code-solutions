b9c228bbaa1ded96c82dc222715e3fbcfd896e99
James Ding <jamesding528@yahoo.com>
2022-05-23 20:29:43 +0000

    shortened invent function

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,21 +1,12 @@
 /* exported invert */
 // create empty object named result
-// create empty array named keys
-// create an empty array named values
-// for each key in source object, add key to the end of keys array and add value of key to the end of values array
-// for each value from index 0 to final index of keys array
-// assign value of keys at current index to property values at current index of result object
+// for each key in source object,
+// assign current key to the value of source at key of the result object
 // return final result object
 function invert(source) {
   var result = {};
-  var keys = [];
-  var values = [];
   for (var key in source) {
-    keys.push(key);
-    values.push(source[key]);
-  }
-  for (var i = 0; i < keys.length; i++) {
-    result[values[i]] = keys[i];
+    result[source[key]] = key;
   }
   return result;
 }
