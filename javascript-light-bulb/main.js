696a28fa0b324c6afa6ba947a6c679ac3816c961
James Ding <jamesding528@yahoo.com>
2022-05-24 22:23:05 +0000

    added pseudocode

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,3 +1,11 @@
+// create counter equal to boolean true
+// queryselect button element and assign it to variable $button
+// queryselect main element and assign it to variable $background
+// define function handleClick with parameter event
+// assign value of not counter to counter
+// if counter is strictly equal to true, assign button-dark to className of button object and assign background-dark to className of background object
+// else if counter is strictly equal to false, assign button-light to className of button object and assign background-light to className of background object
+// add event listener for $button object with arguments 'click and variable handleClick
 var counter = true;
 var $button = document.querySelector('button');
 var $background = document.querySelector('main');
