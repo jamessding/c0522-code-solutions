44dcacb25add28f0ab7276ab65a35ea7128dec8a
James Ding <jamesding528@yahoo.com>
2022-05-25 22:47:47 +0000

    working on unique function

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,14 @@
 /* exported unique */
+function unique(array) {
+  var result = [];
+  result.push(array[0]);
+  for (var i = 1; i < array.length; i++) {
+    for (var k = 0; k < result.length; k++) {
+      if (array[i] !== result[k]) {
+        result.push(array[i]);
+        break;
+      }
+    }
+  }
+  return result;
+}
