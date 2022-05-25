894790088987ece8a807ae2ba2bd162ddcfd1553
James Ding <jamesding528@yahoo.com>
2022-05-25 08:12:26 +0000

    Completed javascript-typing-tutor exercise.

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,34 @@
+var i = 0;
+var counter = 0;
+var startTime;
+var endTime;
+var wordCounter = 1;
+var $spansList = document.querySelectorAll('span');
+var $accuracy = document.querySelector('.accuracy');
+var $time = document.querySelector('.time');
+var $wpm = document.querySelector('.wpm');
+startTime = new Date();
+function handleKey(event) {
+  if ($spansList[i].textContent === ' ') {
+    wordCounter++;
+  }
+  if ($spansList[i].textContent === String.fromCharCode(event.keyCode).toLowerCase()) {
+    $spansList[i].className = 'correct';
+    if (i === $spansList.length - 1) {
+      $accuracy.className = '';
+      $accuracy.textContent += Math.round(($spansList.length - counter) / $spansList.length * 100) + '%';
+      endTime = new Date();
+      var timeDifference = (endTime - startTime) / 1000;
+      $time.className = '';
+      $time.textContent += Math.round(timeDifference) + ' seconds';
+      $wpm.className = '';
+      $wpm.textContent += Math.round(wordCounter / timeDifference * 60);
+    }
+    $spansList[i + 1].className = 'underline';
+    i++;
+  } else {
+    $spansList[i].className = 'incorrect underline';
+    counter++;
+  }
+}
+window.addEventListener('keydown', handleKey);
