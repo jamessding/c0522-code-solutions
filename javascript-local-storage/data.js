cd75fbb390780624495e0a9ce5473043487c6f84
James Ding <jamesding528@yahoo.com>
2022-05-27 22:50:24 +0000

    Completed javascript-local-storage exercise.

---
+++ javascript-local-storage/data.js
@@ -0,0 +1,11 @@
+/* exported todos */
+
+var todos = [];
+window.addEventListener('beforeunload', function (e) {
+  var todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+});
+var previousTodosJSON = localStorage.getItem('javascript-local-storage');
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
