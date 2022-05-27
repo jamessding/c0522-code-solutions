cc90130ddcab2b2e68b5b6aa0a26c340aecfc329
James Ding <jamesding528@yahoo.com>
2022-05-27 19:34:03 +0000

    Completed object-manipulation-3 exercise.

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,12 +1,33 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
-var player = [];
+var playerNames = ['James', 'Nathan', 'Tim', 'Yuhan'];
 var deck = [];
-var counter = 0;
+var players = [];
+var rankNames = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+var suitNames = ['clubs', 'diamonds', 'hearts', 'spades'];
 for (var i = 0; i < 4; i++) {
-  player.push({
-    name: null,
+  players.push({
+    name: playerNames[i],
     hand: null
   });
+  for (var j = 0; j < 13; j++) {
+    deck.push({
+      rank: rankNames[j],
+      suit: suitNames[i]
+    });
+  }
 }
-deck.push(1);
-counter.push(1);
+var shuffledDeck = _.shuffle(deck);
+var handCardNumber = 2;
+var counter = 0;
+for (var k = 0; k < players.length; k++) {
+  for (var l = 0; l < handCardNumber; l++) {
+    if (counter < handCardNumber) {
+      players[k].hand = shuffledDeck[k];
+      _.pull(shuffledDeck, shuffledDeck[k]);
+      counter++;
+    } else {
+      counter = 0;
+    }
+  }
+
+}
