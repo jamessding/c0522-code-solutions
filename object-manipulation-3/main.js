35da0d285d5a14e492b662c4ac1cad3319894fca
James Ding <jamesding528@yahoo.com>
2022-05-27 22:25:58 +0000

    Completed object-manipulation-3 exercise.

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -4,10 +4,11 @@ var deck = [];
 var players = [];
 var rankNames = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
 var suitNames = ['clubs', 'diamonds', 'hearts', 'spades'];
+var handCardNumber = 2;
 for (var i = 0; i < 4; i++) {
   players.push({
     name: playerNames[i],
-    hand: null
+    hand: []
   });
   for (var j = 0; j < 13; j++) {
     deck.push({
@@ -17,17 +18,34 @@ for (var i = 0; i < 4; i++) {
   }
 }
 var shuffledDeck = _.shuffle(deck);
-var handCardNumber = 2;
 var counter = 0;
 for (var k = 0; k < players.length; k++) {
   for (var l = 0; l < handCardNumber; l++) {
-    if (counter < handCardNumber) {
-      players[k].hand = shuffledDeck[k];
-      _.pull(shuffledDeck, shuffledDeck[k]);
-      counter++;
-    } else {
+    players[k].hand.push(shuffledDeck[k]);
+    _.pull(shuffledDeck, shuffledDeck[k]);
+    counter++;
+    if (counter === handCardNumber) {
       counter = 0;
+      break;
     }
   }
-
 }
+var winner = '';
+var winnerCardValue = 0;
+for (var m = 0; m < players.length; m++) {
+  var playerCardValue = 0;
+  for (var n = 0; n < handCardNumber; n++) {
+    if (players[m].hand[n].rank >= 2 && players[m].hand[n].rank <= 10) {
+      playerCardValue += players[m].hand[n].rank;
+    } else if (players[m].hand[n].rank === 'Jack' || players[m].hand[n].rank === 'Queen' || players[m].hand[n].rank === 'King') {
+      playerCardValue += 10;
+    } else if (players[m].hand[n].rank === 'Ace') {
+      playerCardValue += 11;
+    }
+  }
+  if (playerCardValue > winnerCardValue) {
+    winner = players[m];
+    winnerCardValue = playerCardValue;
+  }
+}
+console.log('winner:', winner);
