b162f3427de3a060b864dbf550406d66b036bc5e
James Ding <jamesding528@yahoo.com>
2022-05-30 03:40:05 +0000

    completed optional tiebreaker functionality, increased efficiency by separating code into multiple functions

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,54 +1,64 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
-function cardGame(playerNames, handCardNumber) {
+function cardGame(playerNames, handSize) {
+  playGame(createPlayers(playerNames), createDeck(), handSize);
+}
+function createDeck() {
   var deck = [];
-  var players = [];
   var rankNames = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+  var rankValues = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
   var suitNames = ['clubs', 'diamonds', 'hearts', 'spades'];
   for (var i = 0; i < 4; i++) {
-    players.push({
-      name: playerNames[i],
-      hand: []
-    });
     for (var j = 0; j < 13; j++) {
       deck.push({
         rank: rankNames[j],
-        suit: suitNames[i]
+        suit: suitNames[i],
+        value: rankValues[j]
       });
     }
   }
+  return deck;
+}
+function createPlayers(playerNames) {
+  var players = [];
+  for (var k = 0; k < playerNames.length; k++) {
+    players.push({
+      name: playerNames[k],
+      hand: [],
+      playerCardValue: null
+    });
+  }
+  return players;
+}
+function playGame(players, deck, handSize) {
   var shuffledDeck = _.shuffle(deck);
   var counter = 0;
-  for (var k = 0; k < players.length; k++) {
-    for (var l = 0; l < handCardNumber; l++) {
-      players[k].hand.push(shuffledDeck[k]);
-      _.pull(shuffledDeck, shuffledDeck[k]);
+  var highestHandValue = null;
+  var winners = [];
+  for (var l = 0; l < players.length; l++) {
+    players[l].hand = [];
+    players[l].playerCardValue = null;
+    for (var m = 0; m < handSize; m++) {
+      players[l].hand.push(shuffledDeck[l]);
+      _.pull(shuffledDeck, shuffledDeck[l]);
+      players[l].playerCardValue += shuffledDeck[l].value;
       counter++;
-      if (counter === handCardNumber) {
+      if (counter === handSize) {
         counter = 0;
-        break;
+        if (players[l].playerCardValue > highestHandValue) {
+          highestHandValue = players[l].playerCardValue;
+        }
       }
     }
   }
-  var winner = '';
-  var winnerCardValue = 0;
-  for (var m = 0; m < players.length; m++) {
-    var playerCardValue = 0;
-    for (var n = 0; n < handCardNumber; n++) {
-      if (players[m].hand[n].rank >= 2 && players[m].hand[n].rank <= 10) {
-        playerCardValue += players[m].hand[n].rank;
-      } else if (players[m].hand[n].rank === 'Jack' || players[m].hand[n].rank === 'Queen' || players[m].hand[n].rank === 'King') {
-        playerCardValue += 10;
-      } else if (players[m].hand[n].rank === 'Ace') {
-        playerCardValue += 11;
-      }
-    }
-    if (playerCardValue > winnerCardValue) {
-      winner = players[m];
-      winnerCardValue = playerCardValue;
+  for (var n = 0; n < players.length; n++) {
+    if (players[n].playerCardValue === highestHandValue) {
+      winners.push(players[n]);
     }
   }
-  console.log('winner:', winner.name);
-  console.log('winning cards:', winner.hand);
+  if (winners.length > 1) {
+    playGame(winners, shuffledDeck, handSize);
+  } else {
+    console.log('Winner: ', winners[0]);
+  }
 }
-
 cardGame(['James', 'Nathan', 'Tim', 'Yuhan'], 2);
