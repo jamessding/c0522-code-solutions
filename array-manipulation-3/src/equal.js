4922421832151f912514efb0e6745a1f137019f7
James Ding <jamesding528@yahoo.com>
2022-05-31 18:02:28 +0000

    added pseudocode for equal function

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,4 +1,11 @@
 /* exported equal */
+// initialize counter variable with value 0
+// for each index i from 0 to first length, if first at i is strictly not equal to second at i,
+// return false
+// increment counter
+// if counter is strictly equal to first length and second length,
+// return true
+// else return false
 function equal(first, second) {
   var counter = 0;
   for (var i = 0; i < first.length; i++) {
