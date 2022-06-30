4caf80f064cfa8fc26eef593461a897488f62b16
James Ding <jamesding528@yahoo.com>
2022-06-30 23:22:33 +0000

    Completed node-notes-cli exercise.

---
+++ node-notes-cli/app.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const json = require('./data.json');
+
+if (process.argv[2] === 'read') {
+  for (const key in json.notes) {
+    console.log(`${key}: ${json.notes[key]}`);
+  }
+} else if (process.argv[2] === 'create') {
+  json.notes[json.nextId] = process.argv[3];
+  json.nextId++;
+  const jsonString = JSON.stringify(json, null, 2);
+  fs.writeFile('./data.json', jsonString, err => {
+    if (err) throw err;
+  });
+} else if (process.argv[2] === 'update') {
+  for (const key in json.notes) {
+    if (key === process.argv[3].toString()) {
+      json.notes[key] = process.argv[4];
+    }
+  }
+  const jsonString = JSON.stringify(json, null, 2);
+  fs.writeFile('./data.json', jsonString, err => {
+    if (err) throw err;
+  });
+} else if (process.argv[2] === 'delete') {
+  for (const key in json.notes) {
+    if (key === process.argv[3].toString()) {
+      delete json.notes[key];
+    }
+  }
+  const jsonString = JSON.stringify(json, null, 2);
+  fs.writeFile('./data.json', jsonString, err => {
+    if (err) throw err;
+  });
+}
