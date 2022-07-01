c72e13027c4699544d23f31758c4c79bf3202202
James Ding <jamesding528@yahoo.com>
2022-07-01 00:20:24 +0000

    fixed unecessary for in loop, created keyword variable, and created reusable function for repeating code

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,35 +1,30 @@
 const fs = require('fs');
 const json = require('./data.json');
+const keyword = process.argv[2];
 
-if (process.argv[2] === 'read') {
-  for (var i = 1; i < json.nextId - 1; i++) {
-    console.log(`${i}: ${json.notes[i]}`);
-  }
-} else if (process.argv[2] === 'create') {
-  json.notes[json.nextId] = process.argv[3];
-  json.nextId++;
+function toJSON() {
   const jsonString = JSON.stringify(json, null, 2);
   fs.writeFile('./data.json', jsonString, err => {
     if (err) throw err;
   });
-} else if (process.argv[2] === 'update') {
+}
+
+if (keyword === 'read') {
   for (const key in json.notes) {
-    if (key === process.argv[3].toString()) {
+    console.log(`${key}: ${json.notes[key]}`);
+  }
+} else if (keyword === 'create') {
+  json.notes[json.nextId] = process.argv[3];
+  json.nextId++;
+  toJSON();
+} else if (keyword === 'update') {
+  for (const key in json.notes) {
+    if (key === process.argv[3]) {
       json.notes[key] = process.argv[4];
     }
   }
-  const jsonString = JSON.stringify(json, null, 2);
-  fs.writeFile('./data.json', jsonString, err => {
-    if (err) throw err;
-  });
-} else if (process.argv[2] === 'delete') {
-  for (const key in json.notes) {
-    if (key === process.argv[3].toString()) {
-      delete json.notes[key];
-    }
-  }
-  const jsonString = JSON.stringify(json, null, 2);
-  fs.writeFile('./data.json', jsonString, err => {
-    if (err) throw err;
-  });
+  toJSON();
+} else if (keyword === 'delete') {
+  delete json.notes[process.argv[3]];
+  toJSON();
 }
