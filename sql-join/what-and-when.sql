70aec56a5101b0a4c3924cec498a6496e19ece19
James Ding <jamesding528@yahoo.com>
2022-07-07 18:39:14 +0000

    Completed sql-join exericise.

---
+++ sql-join/what-and-when.sql
@@ -0,0 +1,6 @@
+select "releaseYear",
+       "genres"."name" as "city"
+  from "films"
+  join "filmGenre" using ("filmId")
+  join "genres" using ("genreId")
+ where "title" = 'Boogie Amelie';
---
+++ sql-join/where-are-they.sql
@@ -0,0 +1,8 @@
+select "line1",
+       "cities"."name" as "city",
+       "district",
+       "countries"."name" as "country"
+  from "addresses"
+  join "cities" using ("cityId")
+  join "countries" using ("countryId")
+ limit 10;
