8dc71a371f588141950f932f678ea21166795a12
James Ding <jamesding528@yahoo.com>
2022-07-14 23:41:56 +0000

    Completed react-toggle-switch-component challenge.

---
+++ react-toggle-switch-component/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ToggleSwitch from './toggle-switch.jsx';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<ToggleSwitch />);
