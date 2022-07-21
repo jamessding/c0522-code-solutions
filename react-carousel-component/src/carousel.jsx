34c69bd8260c4544a7d1a2783be893259ae877f3
James Ding <jamesding528@yahoo.com>
2022-07-21 23:40:21 +0000

    Completed react-carousel-component challenge.

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -2,13 +2,65 @@ import React from 'react';
 export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { image: '../images/001.png' };
+    this.state = {
+      index: 0,
+      interval: null
+    };
     this.handleClick = this.handleClick.bind(this);
+    this.createImages = this.createImages.bind(this);
+    this.createDots = this.createDots.bind(this);
   }
 
-  handleClick() {
-    // setState of current image to next one
-    this.setState();
+  handleClick(event) {
+    if (event.target.tagName === 'I') {
+      clearInterval(this.state.interval);
+      this.setState({
+        interval: setInterval(
+          () => this.setState(
+            { index: (this.state.index === this.props.images.length - 1) ? 0 : this.state.index + 1 }),
+          3000
+        )
+      });
+    }
+    if (event.target.className === 'fas fa-chevron-left fa-5x') {
+      this.setState({ index: (this.state.index === 0) ? this.props.images.length - 1 : this.state.index - 1 });
+    } else if (event.target.className === 'fas fa-chevron-right fa-5x') {
+      this.setState({ index: (this.state.index === this.props.images.length - 1) ? 0 : this.state.index + 1 });
+    } else if (event.target.className === 'far fa-circle fa-2x padding') {
+      this.setState({ index: Number(event.target.id) });
+    }
+  }
+
+  createImages(props) {
+    const images = props.images;
+    return images.map(image => {
+      const imageIndex = images.indexOf(image);
+      const imageHidden = (imageIndex === this.state.index) ? '' : 'hidden';
+      return (
+        <img src={images[imageIndex].url} key={imageIndex} className={imageHidden} />
+      );
+    });
+  }
+
+  createDots(props) {
+    const images = props.images;
+    return images.map(image => {
+      const imageIndex = images.indexOf(image);
+      const dotClass = (imageIndex === this.state.index) ? 'fas fa-circle fa-2x padding' : 'far fa-circle fa-2x padding';
+      return (
+        <i id={imageIndex} onClick={this.handleClick} key={imageIndex} className={dotClass} />
+      );
+    });
+  }
+
+  componentDidMount() {
+    this.setState({
+      interval: setInterval(
+        () => this.setState(
+          { index: (this.state.index === this.props.images.length - 1) ? 0 : this.state.index + 1 }),
+        3000
+      )
+    });
   }
 
   render() {
@@ -16,23 +68,23 @@ export default class Carousel extends React.Component {
       <div className="container">
         <div className="row margin-top">
           <div className="column-one-fourth left center">
-            <i className="fas fa-chevron-left fa-5x"></i>
+            <i onClick={this.handleClick} className="fas fa-chevron-left fa-5x"></i>
           </div>
           <div className="column-one-half overflow">
             <div className="row transition-div">
-              <img data-id="0" src="../images/001.png" />
+              {
+                this.createImages(this.props)
+              }
             </div>
           </div>
           <div className="column-one-fourth right center">
-            <i className="fas fa-chevron-right fa-5x"></i>
+            <i onClick={this.handleClick} className="fas fa-chevron-right fa-5x"></i>
           </div>
         </div>
         <div className="row justify-center">
-          <i data-id="0" className="fas fa-circle fa-2x padding"></i>
-          <i data-id="1" className="far fa-circle fa-2x padding"></i>
-          <i data-id="2" className="far fa-circle fa-2x padding"></i>
-          <i data-id="3" className="far fa-circle fa-2x padding"></i>
-          <i data-id="4" className="far fa-circle fa-2x padding"></i>
+          {
+            this.createDots(this.props)
+          }
         </div>
       </div>
     );
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Carousel from './carousel';
 
-const images = ['../images/001.png', '../images/004.png', '../images/007.png', '../images/025.png', '../images/039.png'];
+const images = [
+  { url: '../images/001.png' },
+  { url: '../images/004.png' },
+  { url: '../images/007.png' },
+  { url: '../images/025.png' },
+  { url: '../images/039.png' }
+];
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(<Carousel images={images}/>);
